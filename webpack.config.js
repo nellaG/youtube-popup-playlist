bd108997cf3b57e3087d6a278214385c860274eb
nellag <seirios0107@gmail.com>
2018-04-26 01:20:36 +0900

    use TypeScript (some tutorial codes)

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
 const path = require('path');
 
 module.exports = {
-    entry: './ypopup/static/js/index.js',
+    entry: './ypopup/static/js/index.tsx',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'ypopup/static')
-    }
+    },
+    devtool: "source-map",
+
+    resolve: {
+        extensions:[".ts", ".tsx", ".js", ".json"]
+    },
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
+    },
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
 };
 
 
---
+++ ypopup/static/js/components/Hello.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+
+export interface HelloProps {
+    compiler: string;
+    framework: string;
+}
+
+
+export class Hello extends React.Component<HelloProps, {}> {
+    render() {
+        const { compiler, framework } = this.props;
+        return  <h1> Hello from { compiler } and { framework }</h1>
+    }
+}
